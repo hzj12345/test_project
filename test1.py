584f9ca8d119e06b14e65be3d69ec5bb4c437060
huangzhijie <huangzhijie1@jd.com>
2021-03-17 16:29:05 +0800

    second

--- test1.py
+++ test1.py
@@ -1,14 +1,12 @@
 from pyspark import SparkContext
-sc = SparkContext("local", "Collect app")
-words = sc.parallelize (
-   ["scala",
-   "java",
-   "hadoop",
-   "spark",
-   "akka",
-   "spark vs hadoop",
-   "pyspark",
-   "pyspark and spark"]
-)
-coll = words.collect()
-print("Elements in RDD -> %s" % (coll))
+from operator import add
+sc = SparkContext("local", "first app")
+#nums = sc.parallelize([1, 2, 3, 4, 5])
+#adding = nums.reduce(lambda a,b:a+b)
+lines=sc.textFile('D:\spark-3.1.1-bin-hadoop2.7\README.md')
+#print(lines.first())
+#print(lines.count())
+#lines=lines.take(3)
+print(lines.cartesian(lines).collect())
+
+#print("Adding all the elements -> %i" % (adding))
